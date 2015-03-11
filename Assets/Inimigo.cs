42f99d0abcb2dd5817512671d2447f6b69993fa1
Felipe Torres <felipe.gtorres@gmail.com>
2015-03-11 18:16:20 -0300

    Cap 3: movimentando o inimigo pelo NavMesh.

---
+++ Assets/Inimigo.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Inimigo : MonoBehaviour {
+
+	private NavMeshAgent agente;
+	
+	void Start () {
+		agente = GetComponent<NavMeshAgent>();
+		GameObject fimDoCaminho = GameObject.Find ("FimDoCaminho");
+		Vector3 posicaoDoFimDoCaminho = fimDoCaminho.transform.position;
+		agente.SetDestination (posicaoDoFimDoCaminho);
+	}
+}
