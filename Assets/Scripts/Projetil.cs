6d41fb7822303931069833a2e1e924a02210e52a
Felipe Torres <felipe.gtorres@gmail.com>
2015-03-13 18:01:28 -0300

    Cap 5: disparando vários projeteis.

---
+++ Assets/Scripts/Projetil.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projetil : MonoBehaviour {
+
+	[Range(0,5)]
+	public float velocidade;
+
+	void Update () {
+		Vector3 posicaoAtual = transform.position;
+		Vector3 deslocamento = transform.forward * Time.deltaTime * velocidade;
+		transform.position = posicaoAtual + deslocamento;
+	}
+}
---
+++ Assets/Scripts/TorreController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TorreController : MonoBehaviour {
+
+	public GameObject projetil;
+	private float momentoDoUltimoDisparo;
+
+	[Range(0,3)]
+	public float tempoDeRecarga = 1f;
+
+	void Update () 
+	{
+		float tempoAtual = Time.time;
+		if (tempoAtual > momentoDoUltimoDisparo + tempoDeRecarga) {
+			momentoDoUltimoDisparo = tempoAtual;
+			GameObject pontoDeDisparo = GameObject.Find ("CanhaoDaTorre/PontoDeDisparo");
+			Vector3 posicaoDoPontoDeDisparo = pontoDeDisparo.transform.position;
+			Instantiate (projetil, posicaoDoPontoDeDisparo, Quaternion.identity);
+		}
+	}
+}
