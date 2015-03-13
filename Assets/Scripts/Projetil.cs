9f86d80ac721c44a0a7723700a72ba8a3c85c5e9
Felipe Torres <felipe.gtorres@gmail.com>
2015-03-13 18:18:03 -0300

    Cap 5: missil teleguiado.

--- Assets/Scripts/Projetil.cs
+++ Assets/Scripts/Projetil.cs
@@ -5,10 +5,31 @@ public class Projetil : MonoBehaviour {
 
 	[Range(0,5)]
 	public float velocidade;
+	private GameObject alvo;
 
-	void Update () {
+	void Start () 
+	{
+		alvo = GameObject.Find("Inimigo");
+	}
+
+	void Update () 
+	{
+		anda ();
+		alteraDirecao ();
+	}
+
+	private void anda()
+	{
 		Vector3 posicaoAtual = transform.position;
 		Vector3 deslocamento = transform.forward * Time.deltaTime * velocidade;
 		transform.position = posicaoAtual + deslocamento;
+	}
+
+	private void alteraDirecao() 
+	{
+		Vector3 posicaoAtual = transform.position;
+		Vector3 posicaoDoAlvo = alvo.transform.position;
+		Vector3 direcaoDoAlvo = posicaoDoAlvo - posicaoAtual;
+		transform.rotation = Quaternion.LookRotation (direcaoDoAlvo);
 	}
 }
