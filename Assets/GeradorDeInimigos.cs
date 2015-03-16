b5bfdbd72a44fd40b64de27d4ce96952cfdc1951
Felipe Torres <felipe.gtorres@gmail.com>
2015-03-16 18:36:22 -0300

    Criando varios inimigos.

---
+++ Assets/GeradorDeInimigos.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GeradorDeInimigos : MonoBehaviour 
+{
+
+	public GameObject inimigo;
+	private float momentoDaUltimaGeracao;
+	
+	[Range(0,3)]
+	public float tempoDeCriacao = 2f;
+	
+	void Update () 
+	{
+		float tempoAtual = Time.time;
+		if (tempoAtual > momentoDaUltimaGeracao + tempoDeCriacao) 
+		{
+			momentoDaUltimaGeracao = tempoAtual;
+			Vector3 posicaoDoGerador = this.transform.position;
+			Instantiate (inimigo, posicaoDoGerador, Quaternion.identity);
+		}
+	}}
