4e7e4c9e282cd40c364dcc56e5a39db1a49802ab
Felipe Torres <felipe.gtorres@gmail.com>
2015-03-18 19:55:14 -0300

    Cap 10: Gerando inimigos diferentes na mao.

---
+++ Assets/Scripts/GerenciadorDeInimigos.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GerenciadorDeInimigos : MonoBehaviour 
+{
+
+	[SerializeField]
+	private List<GeradorDeInimigos> geradores;
+
+	void Start ()
+	{
+		Instantiate (geradores [0]);
+	}
+	
+}
