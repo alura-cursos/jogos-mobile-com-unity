b48e0e97642221f786decf605f5011cdb1b97427
Felipe Torres <felipe.gtorres@gmail.com>
2015-04-01 15:59:13 -0300

    Cap 12: Fazendo o botao criar uma nova torre.

---
+++ Assets/Scripts/FabricaDeTorre.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FabricaDeTorre : MonoBehaviour 
+{
+
+	public void criaTorre (GameObject torrePrefab)
+	{
+		Instantiate (torrePrefab);
+	}
+}
