0e9624daac081de51879236320b3816dece4593f
Felipe Torres <felipe.gtorres@gmail.com>
2015-04-14 17:20:15 -0300

    Cap 14: explosao do laser ao atingir inimigo.

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class Laser : MonoBehaviour, Projetil {
 
+	[SerializeField] private GameObject fumacaDoLaserPrefab;
+
 	void Start () 
 	{
 		autoDestroiDepoisDeSegundos (0.1f);
@@ -12,12 +14,20 @@ public class Laser : MonoBehaviour, Projetil {
 	{
 		LineRenderer lineRenderer = GetComponent<LineRenderer> ();
 		GameObject inimigoGameObject = inimigo.gameObject;
+		Vector3 posicaoDoInimigo = inimigoGameObject.transform.position;
 		lineRenderer.SetPosition (0, this.gameObject.transform.position);
-		lineRenderer.SetPosition (1, inimigoGameObject.transform.position);
+		lineRenderer.SetPosition (1, posicaoDoInimigo);
+		Queima (posicaoDoInimigo);
 	}
 
 	private void autoDestroiDepoisDeSegundos(float segundos) 
 	{
 		Destroy (this.gameObject, segundos);
+	}
+
+	private void Queima (Vector3 posicaoDoInimigo) 
+	{
+		GameObject fumaca = Instantiate(fumacaDoLaserPrefab, posicaoDoInimigo, Quaternion.identity) as GameObject;
+		Destroy (fumaca, 1);
 	}
 }
